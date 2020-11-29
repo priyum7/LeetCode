b8ed3c118b94ca4201a6a75b8906663d3f1a9f04
priyum7 <30372297+priyum7@users.noreply.github.com>
2020-11-29 09:46:09 +0530

    working commit - Created using LeetHub

---
+++ invert-binary-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root, TreeNode* &invertedroot)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        
+        if(invertedroot==NULL)
+        {
+            invertedroot=new TreeNode(root->val);
+        }
+        
+        //invertedroot=root;
+        helper(root->left, invertedroot->right);
+        helper(root->right, invertedroot->left);
+    }
+        
+        
+        
+    TreeNode* invertTree(TreeNode* root) {
+        
+        TreeNode* invertedroot=NULL;
+        helper(root, invertedroot);
+        return invertedroot;
+        
+    }
+};
