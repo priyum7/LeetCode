0d238800489992c4515b39600dbeb750dca1e758
Priyum Agrawal <priyumagrawal@gmail.com>
2020-12-28 14:48:24 +0530

    Time: 32 ms (73.97%), Space: 23.4 MB (95.31%) - LeetHub

---
+++ reverse-string/reverse-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        int left=0,right=s.size()-1;
+        
+        while(left<right)
+        {
+            int temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+        
+        
+    }
+};
