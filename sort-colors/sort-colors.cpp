57a028c9abda2850bf35655a15bc08942dc5b11b
Priyum Agrawal <priyumagrawal@gmail.com>
2021-03-20 02:44:27 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (79.08%) - LeetHub

--- sort-colors/sort-colors.cpp
+++ sort-colors/sort-colors.cpp
@@ -1,48 +1,46 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        
-        int left=0,right=nums.size()-1;
-        int i=0;
-        while(i<=right)
-        {
-            if(nums[i]==0)
-            {
-                while(left<=right && nums[left]==0)
-                {
-                    left++;
-                }
-                
-                if(left<i)
-                {
-                    int temp = nums[left];
-                    nums[left] = nums[i];
-                    nums[i] = temp;
-                    continue;
-                }
-            }
-            
-            else if(nums[i]==2)
-            {
-                while(right<nums.size() && nums[right]==2)
-                {
-                    right--;
-                }
-                
-                if(right>i)
-                {
-                    int temp = nums[right];
-                    nums[right] = nums[i];
-                    nums[i] = temp;
-                    continue;
-                }
-            }
-            
-  
-            i++;
-​
-            
-        }
-        
-    }
+    void sortColors(vector<int>& nums) {
+        
+        
+        int left=0, right=nums.size()-1;
+        if(right==0)
+            return;
+        while(left<nums.size() && nums[left]==0)
+        {
+            left++;
+        }
+        
+        while(right>=0 && nums[right]==2)
+        {
+            right--;
+        }
+        
+        int t=left;
+        
+        while(t<=right)
+        {
+            if(nums[t]==0)
+            {
+                swap(nums[t], nums[left]);
+                
+                    left++;
+                if(t>=left)
+                    continue;
+                
+                    
+                
+            }
+            else if(nums[t]==2)
+            {
+                swap(nums[t], nums[right]);
+                
+                    right--;
+                if(t<=right)
+                    continue;
+                
+            }
+            t++;
+        }
+    }
 };
