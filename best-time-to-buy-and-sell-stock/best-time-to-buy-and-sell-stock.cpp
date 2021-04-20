d4041849eb0def2ec8cf1c05a90aa2e5e180609a
Priyum Agrawal <priyumagrawal@gmail.com>
2021-04-20 23:20:18 +0530

    Time: 108 ms (79.78%), Space: 93.3 MB (32.52%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        
-        int n = prices.size();
-        
-        if(n==1)
-            return 0;
-        
-       int left=INT_MAX, right=0;
-        int t=0;
-        while(t<prices.size())
-        {
-            if(left>prices[t])
-            {   
-                int currLeft=prices[t];
-                cout<<currLeft;
-                t++;
-                int currRight=currLeft;
-                while( t<n && prices[t]>=currLeft)
-                {
-                    currRight = max(currRight, prices[t]);
-                    t++;
-                }
-                cout<<" "<<currRight<<endl;
-                if(right-left<currRight-currLeft)
-                {
-                    left=currLeft;
-                    right=currRight;
-                }
-            }
-            else
-            t++;
-        }
-        
-        if(right==0)
-            return 0;
-        else return right-left;
-        
-    }
+    int maxProfit(vector<int>& prices) {
+        
+        if(prices.size()==0)
+            return 0;
+        
+        
+        
+        int min = prices[0] ;
+        int result = 0;
+        
+        for(int t=0;t<prices.size();t++)
+        {
+            if(prices[t]<min)
+                min = prices[t];
+            
+            result=max(result, prices[t]-min);
+        }
+        return result;
+        
+    }
 };
