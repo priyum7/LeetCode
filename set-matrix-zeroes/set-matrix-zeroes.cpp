0f44ab289de7c9225d11cf09d0f72c691204b202
Priyum Agrawal <priyumagrawal@gmail.com>
2021-04-21 02:33:34 +0530

    Time: 16 ms (39.97%), Space: 13.3 MB (38.06%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         
-        unordered_map<int,int> row;
-        unordered_map<int,int> column;
+        bool iscol=false;
+        bool isrow=false;
         
         for(int i=0;i<matrix.size();i++)
         {
@@ -11,22 +11,66 @@ public:
             {
                 if(matrix[i][j]==0)
                 {
-                    row[i]++;
-                    column[j]++;
+                    if(i==0 || j==0)
+                    {
+                        if(i==0 && j==0)
+                        {
+                            isrow=true;
+                            iscol=true;
+                        }
+                        else if(i==0)
+                        {
+                            isrow=true;
+                            matrix[0][j]=0;
+                        }
+                        else
+                        {
+                            iscol=true;
+                            matrix[i][0]=0;        
+                        }
+                    }
+                    else
+                    {
+                    
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                    }
                 }
             } 
         }
         
-        for(int i=0;i<matrix.size();i++)
+        for(int i=matrix[0].size()-1; i>0; i--)
         {
-           for(int j=0;j<matrix[0].size();j++)
+            if(  matrix[0][i]==0)
             {
-                if(row.count(i)>0 || column.count(j)>0)
-                {
-                    matrix[i][j]=0;
-                }
-            } 
+           for(int j=0; j<matrix.size(); j++)
+           {
+               matrix[j][i]=0;
+           }
+            }
         }
+        
+        
+        for(int i=matrix.size()-1;i>=0;i--)
+        {
+            if((i==0 && isrow) || matrix[i][0]==0)
+            {
+               for(int j=0; j<matrix[0].size(); j++)
+               {
+                   matrix[i][j]=0;
+               }
+            }
+        }
+        
+        if(iscol)
+        {
+            for(int j=0; j<matrix.size(); j++)
+           {
+               matrix[j][0]=0;
+           }
+        }
+        
+        
         
     }
 };
