c4b6d3b983965543557bd1a04bccf7da4c1631b8
Priyum Agrawal <priyumagrawal@gmail.com>
2021-04-22 20:14:54 +0530

    Time: 12 ms (47.10%), Space: 13.1 MB (5.11%) - LeetHub

--- longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,47 +1,55 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& nums) {
-        
-        
-        if(nums.size()==0)
-            return 0;
-        
-        unordered_map<int, int> mymap;
-        
-        for(int t=0; t<nums.size(); t++)
-        {
-            mymap[nums[t]]=0;
-        }
-        
-        int result = INT_MIN;
-        
-        for(int t=0; t<nums.size(); t++)
-        {
-           if( mymap[nums[t]]==0)
-           {
-               mymap[nums[t]]=1;
-               int temp = nums[t];
-               int count=1;
-               while(mymap.count(--temp) && mymap[temp]==0)
-               {
-                   mymap[temp]=1;
-                   count++;
-               }
-               temp = nums[t];
-               while(mymap.count(++temp) && mymap[temp]==0)
-               {
-                   mymap[temp]=1;
-                   count++;
-               }
-                if(count>result)
-                result=count;
-           }
-​
-           
-        }
-        
-        return result;
-        
-        
-    }
+    int longestConsecutive(vector<int>& nums) {
+        
+        unordered_map<int, int> visited;
+        unordered_map<int, int> hash;
+        
+        
+        if(nums.size()==0)
+            return 0;
+        
+        for(int t=0;t<nums.size();t++)
+        {
+            hash[nums[t]]++;
+        }
+        
+        int max_length = INT_MIN;
+        
+        for(int t=0;t<nums.size();t++)
+        {
+            if(visited.count(nums[t])==0)
+            {
+                
+                int temp_seq_length = 1;
+                
+                visited[nums[t]]++;
+                
+                int temp_element=nums[t]-1;
+                
+                while(hash.count(temp_element))
+                {
+                    temp_seq_length++;
+                    visited[temp_element]++;
+                    temp_element--;
+                }
+                
+                temp_element = nums[t]+1;
+                
+                while(hash.count(temp_element))
+                {
+                    temp_seq_length++;
+                    visited[temp_element]++;
+                    temp_element++;
+                }
+                
+                if(max_length<temp_seq_length){
+                    max_length = temp_seq_length;
+                }
+            }
+        }
+        
+        return max_length;
+                
+    }
 };
