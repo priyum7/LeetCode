b7a0d7f09dffa3db6944288d4386f519624a67d4
Priyum Agrawal <priyumagrawal@gmail.com>
2021-04-25 09:05:00 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (40.78%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.cpp
+++ search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,31 +1,49 @@
 class Solution {
 public:
     
-    bool helper(int i, int j, vector<vector<int>>& matrix, int target)
+    bool helper(vector<vector<int>>& matrix, int target, int left, int right)
     {
-        
-        
-        if(i>=matrix.size() || j>=matrix[0].size())
+        if(left>right)
+        {
             return false;
-        
-        if(matrix[i][j]==target)
-            return true;
-        
-        if( target <= matrix[i][matrix[0].size()-1] && target>=matrix[i][0])
-        {
-            if(helper(i, j+1, matrix, target))
-                return true;
         }
-        else
-        {
-            if(helper(i+1, 0, matrix, target))
-                return true;
-        }
-        return false;
         
+        else if(left<=right)
+        {
+            int mid = (left+right)/2;
+            int curr = matrix[mid/matrix[0].size()][mid%matrix[0].size()];
+            
+            if(curr==target)
+            {
+                return true;
+            }
+            else if(target<curr)
+            {
+                if(helper(matrix, target, left, mid-1))
+                    return true;
+            }
+            else
+            {
+                 if(helper(matrix, target, mid+1, right))
+                     return true;
+            }
+            
+           
+            
+        
+        }
+         return false;
     }
     
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        return helper(0,0,matrix, target);
+        
+        if(matrix.size()==0)
+            return false;
+        else if(matrix[0][0]==target)
+            return true;
+        else
+        
+        return helper(matrix, target, 0, (matrix.size()*matrix[0].size())-1);
+        
     }
 };
