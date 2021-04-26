9fb381e81c0d22850ae0b3b5e6a7b9a9dc20fa68
Priyum Agrawal <priyumagrawal@gmail.com>
2021-04-26 10:55:51 +0530

    Time: 28 ms (17.89%), Space: 16 MB (14.24%) - LeetHub

--- merge-intervals/merge-intervals.cpp
+++ merge-intervals/merge-intervals.cpp
@@ -1,52 +1,47 @@
 class Solution {
 public:
-    
-    static bool comp(vector<int> &a, vector<int> &b)
-    {
-        return a[0]<b[0];
-        
-    }
-    
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
-        if(intervals.size()==0)
-            return intervals;
-        
-        sort(intervals.begin(),intervals.end(),comp);
-        
-        int t=0,n=intervals.size();
-        vector<vector<int>> result;
-        vector<int> temp;
-        temp = intervals[0];
-        
-        while(t<intervals.size())
-        {
-            
-            if(t==n-1)
-            {
-                result.push_back(intervals[t]);
-                break;
-            }
-            
-            
-            if((temp[1]>=intervals[t+1][0] && temp[1]<=intervals[t+1][1]) || temp[1]>intervals[t+1][1])
-            {
-                temp[1] = max(temp[1],intervals[t+1][1]);
-                t++;
-                if(t==n-1)
-                {
-                    result.push_back(temp);
-                    break;
-                }
-            }
-            else
-            {
-                result.push_back(temp);
-                temp =  intervals[t+1];
-                t++;
-            }
-        }
-        
-        return result;
-    }
+    
+    static bool comp(vector<int> &a, vector<int> &b)
+    {
+        if(a[0]<b[0])
+            return true;
+        else
+            return false;
+    }
+    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        
+    sort(intervals.begin(), intervals.end(), comp);
+        
+        
+    vector<vector<int>> result;        
+    stack<vector<int>> s;
+        
+    for(int i=0;i<intervals.size();i++)
+    {
+        int left ,right;
+        vector<int> curr = intervals[i];
+        while(!s.empty() && ( s.top()[1]>=curr[0] || s.top()[1]>=curr[1]))
+        {
+            vector<int> temp = s.top();
+            s.pop();
+            left = min(temp[0], curr[0]);
+            right = max(temp[1], curr[1]);
+            curr = {left,right};
+        }
+        
+        s.push(curr);
+        
+    }
+        
+        while(s.size()!=0)
+        {
+            result.push_back(s.top());
+            s.pop();
+        }
+        
+        
+        return result;;
+    }
 };
